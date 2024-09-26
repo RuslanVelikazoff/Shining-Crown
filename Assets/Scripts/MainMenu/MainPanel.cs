f59b5fc37dadb92915b4397a84f27fe28afe4185
Ruslan Velikazov <123361452+RuslanVelikazoff@users.noreply.github.com>
2024-09-26 13:39:50 +0300

    Create main panel

--- Assets/Scripts/MainMenu/MainPanel.cs
+++ Assets/Scripts/MainMenu/MainPanel.cs
@@ -12,6 +12,8 @@ public class MainPanel : MonoBehaviour
     [SerializeField] 
     private Button achievementsButton;
     [SerializeField]
+    private Button leaderboardButton;
+    [SerializeField]
     private Button exitButton;
 
     [Space(13)] 
@@ -24,6 +26,8 @@ public class MainPanel : MonoBehaviour
     private GameObject storePanel;
     [SerializeField]
     private GameObject achievementsPanel;
+    [SerializeField] 
+    private GameObject leaderboardPanel;
 
     private void Start()
     {
@@ -65,6 +69,15 @@ public class MainPanel : MonoBehaviour
             {
                 this.gameObject.SetActive(false);
                 achievementsPanel.SetActive(true);
+            });
+        }
+
+        if (leaderboardButton != null)
+        {
+            leaderboardButton.onClick.AddListener(() =>
+            {
+                this.gameObject.SetActive(false);
+                leaderboardPanel.SetActive(true);
             });
         }
 
